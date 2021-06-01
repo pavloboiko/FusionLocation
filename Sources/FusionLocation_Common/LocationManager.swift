133975ec18ab29bb40cc7ae97625c18d0e805146
Pavlo Boiko <boiko.impul@gmail.com>
2021-06-01 22:33:39 +0300

    no message

--- Sources/FusionLocation_Common/LocationManager.swift
+++ Sources/FusionLocation_Common/LocationManager.swift
@@ -1,4 +1,4 @@
-
+import Foundation
 
 public enum LocationUsage {   
   case always
@@ -9,7 +9,6 @@ public protocol LocationManagerProtocol {
     init(usage: LocationUsage) 
 
     func requestAuthorization()
-    func checkAuthorization()
     func requestCurrentLocation(receiver: @escaping (Location) -> Void)
     func startUpdatingLocation(receiver: @escaping (Location) -> Void)
     func stopUpdatingLocation()
