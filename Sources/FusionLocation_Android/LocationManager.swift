0195a655ee227b1d051e5c9444f10d42036f580c
Pavlo Boiko <boiko.impul@gmail.com>
2021-06-08 08:29:27 +0300

    test

--- Sources/FusionLocation_Android/LocationManager.swift
+++ Sources/FusionLocation_Android/LocationManager.swift
@@ -54,7 +54,7 @@ extension LocationManager: LocationManagerProtocol {
   public func requestCurrentLocation(receiver: @escaping (FusionLocation_Common.Location?) -> Void) {    
   	self.locationListener.receiver = receiver  		
    	guard
-    	let provider = self.locationManager?.getBestProvider(criteria: Criteria(), enabledOnly: false)
+    	let provider = self.locationManager?.getBestProvider(criteria: Criteria(), enabledOnly: true)
     else {
       return
     }   
